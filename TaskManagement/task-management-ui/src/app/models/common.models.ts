b2121757cbd0610fd6a214b1964e4b43788cc69c
Youssef <youssefabdelmoniem11@gmail.com>
2026-09-04 21:06:32 +0300

    chore: remove frontend work from backend repo

--- TaskManagement/task-management-ui/src/app/models/common.models.ts
+++
@@ -1,9 +0,0 @@
-export interface PagedResponse<T> {
-  items: T[];
-  page: number;
-  pageSize: number;
-  totalItems: number;
-  totalPages: number;
-  hasNextPage: boolean;
-  hasPreviousPage: boolean;
-}
